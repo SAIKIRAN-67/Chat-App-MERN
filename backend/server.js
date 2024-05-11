052c8d4793daf962391e43fac1c8d5e487a1c40e
Sai Kiran <21pa1a5467@vishnu.edu.in>
2024-05-11 17:35:34 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import messageRoutes from "./routes/message.route.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import userRoutes from "./routes/user.route.js"
 import cookieParser from "cookie-parser";
-const app=express();
+import { app,server } from "./socket/socket.js";
 
 dotenv.config();
 app.use(express.json());
@@ -21,7 +21,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectToMongoDB();
     console.log("server is listening in port 5000 for chat app");
 })
